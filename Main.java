237a4de20268593283e5bcdbc77ed4052d0208ce
kernkarp <kernkarp@gmail.com>
2023-02-08 22:47:26 +0200

    add Main.java and int a,b which are randomly generated in the ranges of 20-30, 40-50, respectively

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = (int) (20+Math.random()*(30-20));
+        int b = (int) (40+Math.random()*(50-40));
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
