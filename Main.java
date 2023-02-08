d82e40cfec4b46ebec6383eb68a3ea7a86313f0c
kernkarp <kernkarp@gmail.com>
2023-02-08 23:13:21 +0200

    add myNumbersProcessing(int a, int b)

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 public class Main {
+    public static void myNumbersProcessing(int a, int b) {
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a * b = " + (a * b));
+    }
+
     public static void main(String[] args) {
         int a = (int) (20+Math.random()*(30-20));
         int b = (int) (40+Math.random()*(50-40));
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+
+        myNumbersProcessing(a, b);
     }
 }
